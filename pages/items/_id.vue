2fff0a1ab9424549eab5a451e6743bf516cc2f54
MyPoZi <kxx555u.u@gmail.com>
2019-08-12 19:36:22 +0900

    items/_idのページ追加

--- pages/items/_id.vue
+++ pages/items/_id.vue
@@ -3,43 +3,9 @@
     <v-flex xs12 sm8 md6>
       <div class="text-center">
         <h1>
-          商品追加
-        </h1>
-        <v-form>
-          <v-container grid-list-xl>
-            <v-layout wrap>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="title"
-                  label="title"
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="description"
-                  label="description"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="price"
-                  type="number"
-                  label="price"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <input type="file" @change="onFileChange" />
-                <img v-show="uploadedImage" :src="uploadedImage" />
-              </v-flex>
-            </v-layout>
-          </v-container>
-          <v-btn class="mr-4" @click="submit">submit</v-btn>
-        </v-form>
-        <h1>
           商品一覧
         </h1>
-        <div v-for="item in reverseItems" :key="item.id">
+        <div>
           <p>タイトル: {{ item.title }}</p>
           <p>説明: {{ item.description }}</p>
           <p>値段: {{ item.price }}</p>
@@ -57,7 +23,7 @@ export default {
       title: '',
       description: '',
       price: 0,
-      items: '',
+      item: '',
       uploadedImage: ''
     }
   },
@@ -67,12 +33,16 @@ export default {
       return this.items.slice().reverse()
     }
   },
-  async asyncData({ app }) {
-    const response = await app.$axios.$get('/items').catch((error) => {
-      console.log('response error', error)
-      return false
-    })
-    return { items: response.data.items }
+  async asyncData({ params, app }) {
+    const response = await app.$axios
+      .$get(`/items/${params.id}`)
+      .catch((error) => {
+        console.log('response error', error)
+        return false
+      })
+    console.log(response.data)
+    console.log(response.data.item)
+    return { item: response.data.item }
   },
   methods: {
     async submit() {
@@ -87,7 +57,7 @@ export default {
         })
         .then((response) => {
           console.log('response data', response)
-          this.items.push(response.data.item)
+          this.items.push(response.data)
         })
         .catch((error) => {
           console.log('response error', error)
